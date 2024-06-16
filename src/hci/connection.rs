dd3e29f11abcf8d0f46c81b332e856df93360777
sidit77 <sidit77@gmail.com>
2024-06-16 15:14:28 +0200

    restructured the connection handler to make it easier to customize

--- src/hci/connection.rs
+++ src/hci/connection.rs
@@ -1,10 +1,14 @@
 use std::collections::BTreeMap;
+use std::future::Future;
 use std::path::{Path, PathBuf};
+use std::pin::Pin;
 use std::sync::Arc;
+use std::task::{Context, Poll};
 
 use bytes::{Bytes, BytesMut};
+use futures_lite::{Stream, StreamExt};
 use instructor::{Buffer, BufferMut};
-use tokio::sync::mpsc::unbounded_channel;
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver};
 use tokio::task::{spawn_blocking, JoinHandle};
 use tokio::{fs, spawn};
 use tracing::{debug, trace, warn};
@@ -12,6 +16,7 @@ use tracing::{debug, trace, warn};
 use crate::ensure;
 use crate::hci::consts::*;
 use crate::hci::{Error, Hci};
+use crate::utils::catch_error;
 
 #[derive(Debug, Clone)]
 pub struct ConnectionManagerBuilder {
@@ -55,29 +60,7 @@ impl ConnectionManagerBuilder {
             Err(err) => return Err(err.into())
         };
 
-        let mut events = {
-            let (tx, rx) = unbounded_channel();
-            hci.register_event_handler(
-                [
-                    EventCode::ConnectionRequest,
-                    // EventCode::ConnectionComplete,
-                    // EventCode::DisconnectionComplete,
-                    EventCode::PinCodeRequest,
-                    EventCode::LinkKeyNotification,
-                    EventCode::LinkKeyRequest,
-                    EventCode::IoCapabilityRequest,
-                    EventCode::IoCapabilityResponse,
-                    EventCode::UserConfirmationRequest,
-                    EventCode::UserPasskeyNotification,
-                    EventCode::UserPasskeyRequest,
-                    EventCode::KeypressNotification,
-                    EventCode::RemoteOobDataRequest,
-                    EventCode::SimplePairingComplete
-                ],
-                tx
-            )?;
-            rx
-        };
+        let mut events = ConnectionEventReceiver::new(&hci)?;
 
         if self.simple_secure_pairing {
             hci.set_simple_pairing_support(true).await?;
@@ -108,34 +91,22 @@ struct ConnectionManagerState {
 }
 
 impl ConnectionManagerState {
-    async fn handle_event(&mut self, (code, mut data): (EventCode, Bytes)) -> Result<(), Error> {
-        match code {
-            EventCode::ConnectionRequest => {
-                // ([Vol 4] Part E, Section 7.7.4).
-                let addr: RemoteAddr = data.read_le()?;
-                let _class: ClassOfDevice = data.read_le()?;
-                let link_type: LinkType = data.read_le()?;
-                data.finish()?;
-
+    async fn handle_event(&mut self, event: ConnectionEvent) -> Result<(), Error> {
+        match event {
+            ConnectionEvent::ConnectionComplete { .. } => {}
+            ConnectionEvent::DisconnectionComplete { .. } => {}
+            ConnectionEvent::ConnectionRequest { addr, link_type, .. } => {
                 ensure!(link_type == LinkType::Acl, "Invalid link type");
                 debug!("Connection request: {}", addr);
                 self.hci
                     .accept_connection_request(addr, Role::Slave)
                     .await?;
             }
-            EventCode::PinCodeRequest => {
-                // ([Vol 4] Part E, Section 7.7.22).
-                let addr: RemoteAddr = data.read_le()?;
-                data.finish()?;
-
+            ConnectionEvent::PinCodeRequest { addr } => {
                 debug!("Pin code request: {}", addr);
                 self.hci.pin_code_request_reply(addr, "0000").await?;
             }
-            EventCode::LinkKeyRequest => {
-                // ([Vol 4] Part E, Section 7.7.23).
-                let addr: RemoteAddr = data.read_le()?;
-                data.finish()?;
-
+            ConnectionEvent::LinkKeyRequest { addr } => {
                 debug!("Link key request: {}", addr);
                 if let Some(key) = self.link_keys.get(&addr) {
                     debug!("   Link key present");
@@ -145,22 +116,12 @@ impl ConnectionManagerState {
                     self.hci.link_key_not_present(addr).await?;
                 }
             }
-            EventCode::LinkKeyNotification => {
-                // ([Vol 4] Part E, Section 7.7.24).
-                let addr: RemoteAddr = data.read_le()?;
-                let key: LinkKey = data.read_le()?;
-                let key_type: LinkKeyType = data.read_le()?;
-                data.finish()?;
-
+            ConnectionEvent::LinkKeyNotification { addr, key, key_type } => {
                 debug!("Link key notification: {} {:?} {:?}", addr, key, key_type);
                 self.link_keys.insert(addr, key);
                 self.save_link_keys();
             }
-            EventCode::IoCapabilityRequest => {
-                // ([Vol 4] Part E, Section 7.7.40).
-                let addr: RemoteAddr = data.read_le()?;
-                data.finish()?;
-
+            ConnectionEvent::IoCapabilityRequest { addr} => {
                 debug!("Io capability request: {}", addr);
                 self.hci
                     .io_capability_reply(
@@ -171,70 +132,31 @@ impl ConnectionManagerState {
                     )
                     .await?;
             }
-            EventCode::IoCapabilityResponse => {
-                // ([Vol 4] Part E, Section 7.7.30).
-                let addr: RemoteAddr = data.read_le()?;
-                let io: IoCapability = data.read_le()?;
-                let oob: bool = data.read_le()?;
-                let auth: AuthenticationRequirements = data.read_le()?;
-                data.finish()?;
-
+            ConnectionEvent::IoCapabilityResponse { addr, io, oob, auth } => {
                 debug!("Io capability response: {} {:?} {} {:?}", addr, io, oob, auth);
             }
-            EventCode::UserConfirmationRequest => {
-                // ([Vol 4] Part E, Section 7.7.31).
-                let addr: RemoteAddr = data.read_le()?;
-                let passkey: u32 = data.read_le()?;
-                ensure!(passkey <= 999999, "Invalid passkey");
-                data.finish()?;
-
+            ConnectionEvent::UserConfirmationRequest { addr, passkey } => {
                 debug!("User confirmation request: {} {}", addr, passkey);
                 self.hci.user_confirmation_request_accept(addr).await?;
             }
-            EventCode::SimplePairingComplete => {
-                // ([Vol 4] Part E, Section 7.7.45).
-                let status: Status = data.read_le()?;
-                let addr: RemoteAddr = data.read_le()?;
-                data.finish()?;
-
+            ConnectionEvent::SimplePairingComplete { status, addr } => {
                 debug!("Simple pairing complete: {} {}", addr, status);
             }
-
-            EventCode::UserPasskeyNotification => {
-                // ([Vol 4] Part E, Section 7.7.48).
-                let addr: RemoteAddr = data.read_le()?;
-                let passkey: u32 = data.read_le()?;
-                ensure!(passkey <= 999999, "Invalid passkey");
-                data.finish()?;
-
+            ConnectionEvent::UserPasskeyNotification { addr, passkey } => {
                 debug!("User passkey notification: {} {}", addr, passkey);
                 panic!("Passkeys not supported");
             }
-            EventCode::UserPasskeyRequest => {
-                // ([Vol 4] Part E, Section 7.7.43).
-                let addr: RemoteAddr = data.read_le()?;
-                data.finish()?;
-
+            ConnectionEvent::UserPasskeyRequest { addr } => {
                 debug!("User passkey request: {}", addr);
                 panic!("Passkeys not supported");
             }
-            EventCode::KeypressNotification => {
-                // ([Vol 4] Part E, Section 7.7.49).
-                let addr: RemoteAddr = data.read_le()?;
-                let ty: KeypressNotificationType = data.read_le()?;
-                data.finish()?;
-
+            ConnectionEvent::KeypressNotification { addr, ty } => {
                 debug!("Keypress notification: {} {:?}", addr, ty);
             }
-            EventCode::RemoteOobDataRequest => {
-                // ([Vol 4] Part E, Section 7.7.44).
-                let addr: RemoteAddr = data.read_le()?;
-                data.finish()?;
-
+            ConnectionEvent::RemoteOobDataRequest { addr } => {
                 debug!("Remote OOB data request: {}", addr);
                 panic!("OOB data not supported");
             }
-            _ => unreachable!()
         }
         Ok(())
     }
@@ -248,5 +170,229 @@ impl ConnectionManagerState {
         let data = data.freeze();
         let path = self.link_key_store.clone();
         spawn_blocking(move || std::fs::write(path, &data).unwrap_or_else(|err| warn!("Failed to save link keys: {:?}", err)));
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum ConnectionEvent {
+    // ([Vol 4] Part E, Section 7.7.3).
+    ConnectionComplete {
+        status: Status,
+        handle: u16,
+        addr: RemoteAddr,
+        link_type: LinkType,
+        encryption_enabled: bool
+    },
+    // ([Vol 4] Part E, Section 7.7.4).
+    ConnectionRequest {
+        addr: RemoteAddr,
+        class: ClassOfDevice,
+        link_type: LinkType
+    },
+    // ([Vol 4] Part E, Section 7.7.5).
+    DisconnectionComplete {
+        status: Status,
+        handle: u16,
+        reason: Status
+    },
+    // ([Vol 4] Part E, Section 7.7.22)
+    PinCodeRequest {
+        addr: RemoteAddr
+    },
+    // ([Vol 4] Part E, Section 7.7.23).
+    LinkKeyRequest {
+        addr: RemoteAddr
+    },
+    // ([Vol 4] Part E, Section 7.7.24).
+    LinkKeyNotification {
+        addr: RemoteAddr,
+        key: LinkKey,
+        key_type: LinkKeyType
+    },
+    // ([Vol 4] Part E, Section 7.7.40).
+    IoCapabilityRequest {
+        addr: RemoteAddr
+    },
+    // ([Vol 4] Part E, Section 7.7.30).
+    IoCapabilityResponse {
+        addr: RemoteAddr,
+        io: IoCapability,
+        oob: bool,
+        auth: AuthenticationRequirements
+    },
+    // ([Vol 4] Part E, Section 7.7.31).
+    UserConfirmationRequest {
+        addr: RemoteAddr,
+        passkey: u32
+    },
+    // ([Vol 4] Part E, Section 7.7.48).
+    UserPasskeyNotification {
+        addr: RemoteAddr,
+        passkey: u32
+    },
+    // ([Vol 4] Part E, Section 7.7.43).
+    UserPasskeyRequest {
+        addr: RemoteAddr
+    },
+    // ([Vol 4] Part E, Section 7.7.49).
+    KeypressNotification {
+        addr: RemoteAddr,
+        ty: KeypressNotificationType
+    },
+    // ([Vol 4] Part E, Section 7.7.44).
+    RemoteOobDataRequest {
+        addr: RemoteAddr
+    },
+    // ([Vol 4] Part E, Section 7.7.45).
+    SimplePairingComplete {
+        status: Status,
+        addr: RemoteAddr
+    }
+}
+
+pub struct ConnectionEventReceiver(UnboundedReceiver<(EventCode, Bytes)>);
+
+impl ConnectionEventReceiver {
+    pub fn new(hci: &Hci) -> Result<Self, Error> {
+        let events = {
+            let (tx, rx) = unbounded_channel();
+            hci.register_event_handler(
+                [
+                    EventCode::ConnectionRequest,
+                    EventCode::ConnectionComplete,
+                    EventCode::DisconnectionComplete,
+                    EventCode::PinCodeRequest,
+                    EventCode::LinkKeyNotification,
+                    EventCode::LinkKeyRequest,
+                    EventCode::IoCapabilityRequest,
+                    EventCode::IoCapabilityResponse,
+                    EventCode::UserConfirmationRequest,
+                    EventCode::UserPasskeyNotification,
+                    EventCode::UserPasskeyRequest,
+                    EventCode::KeypressNotification,
+                    EventCode::RemoteOobDataRequest,
+                    EventCode::SimplePairingComplete
+                ],
+                tx
+            )?;
+            trace!("Registered new connection event listener");
+            rx
+        };
+        Ok(Self(events))
+    }
+
+    pub fn recv(&mut self) -> impl Future<Output=Option<ConnectionEvent>> + '_ {
+        //poll_fn(move |cx| self.poll_recv(cx))
+        self.next()
+    }
+
+}
+
+impl Stream for ConnectionEventReceiver {
+    type Item = ConnectionEvent;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        while let Poll::Ready(event) = self.0.poll_recv(cx) {
+            let Some((code, mut data)) = event else {
+                return Poll::Ready(None);
+            };
+            let event: Result<_, instructor::Error> = catch_error(|| match code {
+                EventCode::ConnectionComplete => {
+                    let status: Status = data.read_le()?;
+                    let handle: u16 = data.read_le()?;
+                    let addr: RemoteAddr = data.read_le()?;
+                    let link_type: LinkType = data.read_le()?;
+                    let encryption_enabled: bool = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::ConnectionComplete { status, handle, addr, link_type, encryption_enabled })
+                }
+                EventCode::DisconnectionComplete => {
+                    let status: Status = data.read_le()?;
+                    let handle: u16 = data.read_le()?;
+                    let reason: Status = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::DisconnectionComplete { status, handle, reason })
+                }
+                EventCode::ConnectionRequest => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    let class: ClassOfDevice = data.read_le()?;
+                    let link_type: LinkType = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::ConnectionRequest { addr, class, link_type })
+                }
+                EventCode::PinCodeRequest => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::PinCodeRequest { addr })
+                }
+                EventCode::LinkKeyRequest => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::LinkKeyRequest { addr })
+                }
+                EventCode::LinkKeyNotification => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    let key: LinkKey = data.read_le()?;
+                    let key_type: LinkKeyType = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::LinkKeyNotification { addr, key, key_type })
+                }
+                EventCode::IoCapabilityRequest => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::IoCapabilityRequest { addr })
+                }
+                EventCode::IoCapabilityResponse => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    let io: IoCapability = data.read_le()?;
+                    let oob: bool = data.read_le()?;
+                    let auth: AuthenticationRequirements = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::IoCapabilityResponse { addr, io, oob, auth })
+                }
+                EventCode::UserConfirmationRequest => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    let passkey: u32 = data.read_le()?;
+                    ensure!(passkey <= 999999, instructor::Error::InvalidValue);
+                    data.finish()?;
+                    Ok(ConnectionEvent::UserConfirmationRequest { addr, passkey })
+                }
+                EventCode::SimplePairingComplete => {
+                    let status: Status = data.read_le()?;
+                    let addr: RemoteAddr = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::SimplePairingComplete { status, addr })
+                }
+                EventCode::UserPasskeyNotification => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    let passkey: u32 = data.read_le()?;
+                    ensure!(passkey <= 999999, instructor::Error::InvalidValue);
+                    data.finish()?;
+                    Ok(ConnectionEvent::UserPasskeyNotification { addr, passkey })
+                }
+                EventCode::UserPasskeyRequest => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::UserPasskeyRequest { addr })
+                }
+                EventCode::KeypressNotification => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    let ty: KeypressNotificationType = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::KeypressNotification { addr, ty })
+                }
+                EventCode::RemoteOobDataRequest => {
+                    let addr: RemoteAddr = data.read_le()?;
+                    data.finish()?;
+                    Ok(ConnectionEvent::RemoteOobDataRequest { addr })
+                }
+                _ => unreachable!()
+            });
+            match event {
+                Ok(event) => return Poll::Ready(Some(event)),
+                Err(err) => warn!("Error parsing connection event: {:?}", err)
+            }
+        }
+        Poll::Pending
     }
 }
